e5339e2da550b29d749205213ec2357d3457da85
TungChew03 <130131645+TungChew03@users.noreply.github.com>
2023-05-08 14:04:26 +0800

    Update TableStu2.js

--- frontend/src/layout/TableStu2.js
+++ frontend/src/layout/TableStu2.js
@@ -1,23 +1,31 @@
 import style from '@/styles/Home.module.css'
 import{ useEffect, useState } from 'react'
 
-const data = [
-    { id: 1, name: "John", age: 25 },
-    { id: 2, name: "Jane", age: 30 },
-    { id: 3, name: "Mike", age: 35 },
-  ];
+
 export default function Home() {
   const DataList = () =>{
     const [data, setData]= useState([]);
+    useEffect(()=>{
+      const fetchData = async()=>{
+        try{
+          const response = await fetch ('/api/data');
+          const jsonData = await response.json();
+          setData(jsonData);
+        }catch(error){
+          console.log('Error fetching data: ',error);
+      }
+    };
+    fetchData();
+    },[]);
   }
     return (
         
               <div className={style.container}>
-                {data.map((row) => (
-                  <div className={style.row1} key={row.id}>
+                {data.map((item) => (
+                  <div className={style.row1} key={item.id}>
                     <div className={style.image}></div>
-                    <div>Name <br/>TP<br/>UCDF</div>                    
-                    <div><b>Title</b><br/><br/>Supervisor</div>                  
+                    <div>{item.name}<br/>TP<br/>UCDF</div>                    
+                    <div><b>{item.title}</b><br/><br/>{item.supervisor}</div>                  
                   </div>
                 ))}
               </div>
