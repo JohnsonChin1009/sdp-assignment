3d58dc479a5e1b3b67dbab79b1eac672e1c4ad53
ProPenguinsCKN <chiangkaini@gmail.com>
2023-05-09 13:11:19 +0800

    Update TableStu2.js

--- frontend/src/layout/TableStu2.js
+++ frontend/src/layout/TableStu2.js
@@ -1,31 +1,36 @@
 import style from '@/styles/Home.module.css'
-import{ useEffect, useState } from 'react'
-
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const DataList = () =>{
-    const [data, setData]= useState([]);
-    useEffect(()=>{
-      const fetchData = async()=>{
-        try{
-          const response = await fetch ('http:localhost:8000/api/students');
+  const DataList = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:8000/api/students');
           const jsonData = await response.json();
           setData(jsonData);
-        }catch(error){
-          console.log('Error fetching data: ',error);
-      }
-    };
-    fetchData();
-    },[]);
-  }
-    return (        
+        } catch (error) {
+          console.log('Error fetching data: ', error);
+        }
+      };
+
+      fetchData();
+    }, []);
+
+    return (
       <div className={style.container}>
         {data.map((item) => (
           <div className={style.row1} key={item.id}>
             <div className={style.image}></div>
-            <div>{item.name}<br/>{item.tp_number}<br/>{item.field_of_study}</div>                    
-            <div><b>{item.fyp_title}</b><br/><br/>{item.supervisor}</div>                  
+            <div>{item.name}<br/>{item.tp_number}<br/>{item.field_of_study}</div>
+            <div><b>{item.fyp_title}</b><br/><br/>{item.supervisor}</div>
           </div>
         ))}
-      </div>                           
-    )}
+      </div>
+    );
+  };
+
+  return <DataList />;
+}
