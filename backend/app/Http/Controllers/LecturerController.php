e5f66b8873ec1c85e28660f37c40e5ee1c307770
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-05-10 23:39:23 +0800

    Lecturer Controller

--- backend/app/Http/Controllers/LecturerController.php
+++ backend/app/Http/Controllers/LecturerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Lecturer;
 
 class LecturerController extends Controller
 {
@@ -19,5 +20,23 @@ class LecturerController extends Controller
             'error' => $e->getMessage(),
         ], 500);
     }
-}
+    }
+    public function displayLecturerProfile(Request $request)
+        {
+            $token = $request->header('Authorization');
+
+            $lecturer = lecturer::where('email', $token)->first();
+
+            if(!$lecturer) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $token,
+                ], 401);
+            }
+
+            return respoonse()->json([
+                'success' => true,
+                'data' => $lecturer,
+                ]);
+            }
 }
