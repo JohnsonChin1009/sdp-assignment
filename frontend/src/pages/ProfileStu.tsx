8d6895bfb64dd162008fc179d6702a1be4954f5c
TungChew03 <130131645+TungChew03@users.noreply.github.com>
2023-05-10 09:16:34 +0800

    Update ProfileStu.tsx

--- frontend/src/pages/ProfileStu.tsx
+++ frontend/src/pages/ProfileStu.tsx
@@ -66,7 +66,7 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.contentbox3}>
-            <Table />
+            <Table /><br />
       </div>
       
       <br /><br /><br />
