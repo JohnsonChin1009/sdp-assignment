865d29229e20bb7d0c5c5179db78821111e25162
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-05-12 16:16:51 +0800

    Update generateSeederScript.php

--- backend/tools/generateSeederScript.php
+++ backend/tools/generateSeederScript.php
@@ -4,7 +4,7 @@
     include_once 'generateSeederData.php';
     //Copy the file path to use it
     //Or insert the CSV files within the same directory and change the file name
-    $csvFile = 'C:\Users\shado\Desktop\sdp-assignment\backend\tools\LecturerTableSeeder.csv';
+    $csvFile = 'C:\Users\shado\Desktop\sdp-assignment\backend\tools\StudentTableSeeder.csv';
     $seederFile = 'C:\Users\shado\Desktop\sdp-assignment\backend\tools\Seeder.php';
 
     generateSeederData($csvFile, $seederFile);
